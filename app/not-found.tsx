10d20a9bf9085ec23e3f8d6beb8bdc1fb9954dc3
coderlaari <lauri.letonsaari@gmail.com>
2025-05-02 13:20:08 +0300

    Custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+// app/not-found.tsx
+
+export default function NotFound() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      <p className="mt-4 text-lg">
+        Sorry, we couldn't find what you were looking for.
+      </p>
+    </div>
+  );
+}
